c4e598d51ff2fc357ac6d568fcb806674fbfc173
Mosen501 <mosenaag@gmail.com>
2025-07-13 15:23:21 -0400

    SQL101: dump schema and seed data

---
+++ sql101/scripts/schema.sql
@@ -0,0 +1,99 @@
+-- MySQL dump 10.13  Distrib 9.3.0, for macos15.2 (arm64)
+--
+-- Host: localhost    Database: Excellence_School
+-- ------------------------------------------------------
+-- Server version	9.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `Excellence_School`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `Excellence_School` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `Excellence_School`;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `student_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `student_name` varchar(255) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  `date_of_enrollment` date NOT NULL,
+  `student_email` varchar(255) DEFAULT NULL,
+  `academic_level` tinyint unsigned NOT NULL,
+  `track` varchar(10) NOT NULL,
+  `cumulative_gpa` decimal(5,0) NOT NULL,
+  PRIMARY KEY (`student_id`),
+  UNIQUE KEY `student_email` (`student_email`),
+  CONSTRAINT `students_chk_1` CHECK ((`gender` in (_utf8mb4'F',_utf8mb4'M'))),
+  CONSTRAINT `students_chk_2` CHECK ((`academic_level` between 1 and 6)),
+  CONSTRAINT `students_chk_3` CHECK ((`track` in (_utf8mb4'scientific',_utf8mb4'humanities'))),
+  CONSTRAINT `students_chk_4` CHECK ((`cumulative_gpa` between 0 and 100))
+) ENGINE=InnoDB AUTO_INCREMENT=100030 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `subjects`
+--
+
+DROP TABLE IF EXISTS `subjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subjects` (
+  `subject_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `subject_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=300006 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teachers` (
+  `teacher_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `teacher_name` varchar(255) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  `teacher_email` varchar(255) DEFAULT NULL,
+  `office_number` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`teacher_id`),
+  UNIQUE KEY `teacher_email` (`teacher_email`),
+  CONSTRAINT `teachers_chk_1` CHECK ((`gender` in (_utf8mb4'F',_utf8mb4'M')))
+) ENGINE=InnoDB AUTO_INCREMENT=200010 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'Excellence_School'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-13 15:20:17
