fb95c93796c571bee79039f6997c1ad6f6c5b208
Mosen501 <mosenaag@gmail.com>
2025-07-15 00:41:29 -0400

    second update for schema & seed_data

--- sql101/scripts/schema.sql
+++ sql101/scripts/schema.sql
@@ -24,6 +24,20 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `Excellence_School` /*!40100 DEFAULT CH
 USE `Excellence_School`;
 
 --
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `subject_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `subject_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=300006 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `students`
 --
 
@@ -34,7 +48,7 @@ CREATE TABLE `students` (
   `student_id` int unsigned NOT NULL AUTO_INCREMENT,
   `student_name` varchar(255) NOT NULL,
   `date_of_birth` date NOT NULL,
-  `gender` char(1) NOT NULL,
+  `gender` char(1) DEFAULT NULL,
   `date_of_enrollment` date NOT NULL,
   `student_email` varchar(255) DEFAULT NULL,
   `academic_level` tinyint unsigned NOT NULL,
@@ -46,21 +60,7 @@ CREATE TABLE `students` (
   CONSTRAINT `students_chk_2` CHECK ((`academic_level` between 1 and 6)),
   CONSTRAINT `students_chk_3` CHECK ((`track` in (_utf8mb4'scientific',_utf8mb4'humanities'))),
   CONSTRAINT `students_chk_4` CHECK ((`cumulative_gpa` between 0 and 100))
-) ENGINE=InnoDB AUTO_INCREMENT=100030 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `subjects`
---
-
-DROP TABLE IF EXISTS `subjects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `subjects` (
-  `subject_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `subject_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`subject_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=300006 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=100035 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +74,7 @@ CREATE TABLE `teachers` (
   `teacher_id` int unsigned NOT NULL AUTO_INCREMENT,
   `teacher_name` varchar(255) NOT NULL,
   `date_of_birth` date NOT NULL,
-  `gender` char(1) NOT NULL,
+  `gender` char(6) NOT NULL,
   `teacher_email` varchar(255) DEFAULT NULL,
   `office_number` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`teacher_id`),
@@ -96,4 +96,4 @@ CREATE TABLE `teachers` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-13 15:20:17
+-- Dump completed on 2025-07-15  0:40:39
